7186a06ced4cbaed1b11db8bec406cfe2d98ffca
keagancoding <110374089+keagancoding@users.noreply.github.com>
2024-04-27 22:03:40 -0700

    fix: forgot to remove env.IPFS_KEY import

--- src/app/actions/files.ts
+++ src/app/actions/files.ts
@@ -1,28 +1,26 @@
-"use server"
+// "use server"
 
-import { env } from '@/env';
-import { createThirdwebClient } from "thirdweb";
-import { upload } from "thirdweb/storage";
+// import { env } from '@/env';
+// import { createThirdwebClient } from "thirdweb";
+// import { upload } from "thirdweb/storage";
 
 
-export const uploadToIpfs = async (file: string) => {
-    // convert string toDataURL to png for upload
+// export const uploadToIpfs = async (file: string) => {
+//     const buffer = Buffer.from(file.split(",")[1], "base64");
 
-    const buffer = Buffer.from(file.split(",")[1], "base64");
+//     try {
+//         const client = createThirdwebClient({
+//             secretKey: env.IPFS_KEY
+//         })
 
-    try {
-        const client = createThirdwebClient({
-            secretKey: env.IPFS_KEY
-        })
+//         const uris = await upload({
+//             client,
+//             files: [buffer],
+//             uploadWithoutDirectory: true,
+//         })
 
-        const uris = await upload({
-            client,
-            files: [buffer],
-            uploadWithoutDirectory: true,
-        })
-
-        return uris;
-    } catch (error) {
-        console.log(error);
-    }
-};
+//         return uris;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
